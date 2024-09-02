177e033d6a0115efa9cd0872b2b5582a9d97e056
Ishita <ishitajain385@gmail.com>
2024-09-02 19:58:18 +0530

    made some changes in UI to achieve some more functionalities

--- src/Input.jsx
+++ src/Input.jsx
@@ -10,18 +10,16 @@ export default function Input() {
   const[err,setErr] = useState(false)
   const[err1,setErr1] = useState(false)
  
-let {a}= useSelector(state=>state)
-console.log(a)
-let {b}= useSelector(state=>state)
-console.log(b)
+const dispatch= useDispatch()
   function settingA(e){
 let val = e.target.value
 if(isNaN(val) ){
 setErr(true)
 }
 else{
-a= val
   setvalA(val)
+
+ 
   setErr(false)
 }
   }
@@ -33,17 +31,24 @@ a= val
     setErr1(true)
     }
     else{
-      b=val
       setvalB(val)
+     
+   
       setErr1(false)
     }
       }
-  console.log(valA,valB)
+
   return (
     <div className='flex flex-col'>
       <div>
       <button className='ml-[500px] text-3xl mt-[8px]'>value of A :</button>
       <input type="text" className='rounded-[8px] h-[50px] w-[200px] border-[3px] border-gray-500 ml-[60px] mt-[50px] ' onChange={settingA}/>
+      <button onClick={()=>{
+        dispatch({
+          type:'set_a',
+          payload:valA
+        })
+      }} className='text-2xl ml-[20px] border border-black p-[10px] bg-gray-100'>Set A</button>
       {
         err&&<p className='ml-[720px] text-red-500'>Enter a valid number</p>
       }
@@ -51,6 +56,12 @@ a= val
     <div>
     <button className='ml-[500px] text-3xl mt-[8px]'>value of B :</button>
     <input type="text" className='rounded-[8px] h-[50px] w-[200px] border-[3px] border-gray-500 ml-[60px] mt-[50px]' onChange={settingB}/>
+    <button onClick={()=>{
+       dispatch({
+        type:'set_b',
+        payload:valB
+      })
+    }} className='text-2xl ml-[20px] border border-black p-[10px] bg-gray-100' >Set B</button>
     {
         err1&&<p className='ml-[720px] text-red-500'>Enter a valid number</p>
       }
--- src/Store.jsx
+++ src/Store.jsx
@@ -1,17 +1,26 @@
 
+import { useState } from "react";
 import { createStore } from "redux";
 
 const initialState={
-    a:"",
-    b:"",
+a:0,
+    b:0,
  val:0
    
 
 }
 let reducerfn=(state=initialState,action)=>{
-    console.log(action)
+    console.log(action,state.a,state.b)
 switch(action.type){
-  case  '+' : return (parseInt(state.a)+parseInt(state.b))
+case 'set_a': 
+case 'set_b':
+  case  '+' :
+    let a= parseInt(state.a) ||0
+    let b= parseInt(state.b) ||0
+  return {
+    ...state,
+    val: a+b
+  }
     // case '-': return difference(a,b)
     //     case '*': return multiply(a,b)
     //         case '%': return divide(a,b)
